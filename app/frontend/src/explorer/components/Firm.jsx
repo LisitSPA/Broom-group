a2f640d4ccea2eb7762b99e0d0890f96d7965acc
MafeDuran <mduran@lisit.cl>
2023-11-19 19:16:53 -0300

    cambiods firm

--- app/frontend/src/explorer/components/Firm.jsx
+++ app/frontend/src/explorer/components/Firm.jsx
@@ -6,18 +6,25 @@ import { useSelector, useDispatch } from 'react-redux';
 import { callFirm } from '@/redux/actions/firms';
 
 const translateLevels = (ownersMap, firms) => {
+  console.log(ownersMap, firms)
   if (ownersMap) {
-    const { levels } = ownersMap;
+    const { levels } = ownersMap
 
     // levels.forEach(level => {
 
     // })
   }
-};
+}
 
-const Firm = ({firm}) => {
+const Firm = React.memo(function Firm({ firm, searchTerm, selectAllChecked }) {
+  console.log('Firm se está renderizando. searchTerm:', searchTerm);
+  console.log('Firm renderizado. selectAllChecked:', selectAllChecked);
+
   const dispatch = useDispatch();
-  const { firmOwnersMap, actualVersion } = useSelector((state) => state);
+  const { 
+    firmOwnersMap,
+    actualVersion
+  } = useSelector(state => state);
   const { response } = firmOwnersMap;
   let firms = response ? response.firms : [];
 
@@ -39,30 +46,27 @@ const Firm = ({firm}) => {
   }
 
   const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   useEffect(() => {
-    translateLevels(firmStructure, firms);
+    translateLevels(firmStructure, firms)
     if (!firmStructure) {
-      dispatch(callFirm(firmId));
+      dispatch(callFirm(firmId))
     }
-  }, [isOpen]);
+  }, [isOpen])
 
   useEffect(() => {
     if (!firmStructure && response.firmId === firmId) {
-      setFirmStructure(response.ownersMap);
+      setFirmStructure(response.ownersMap)
     }
-  }, [response]);
+  }, [response])
 
-  const classes = classNames(
-    "flex justify-between items-center p-6 z-20 rounded-md",
-    {
-      "bg-LightGray rounded-b-none": isOpen,
-      "bg-white": !isOpen,
-      "outline outline-1 outline-sky-400": isChecked,
-    }
-  );
+  const classes = classNames('flex justify-between items-center p-6 z-20 rounded-md', {
+    'bg-LightGray rounded-b-none': isOpen,
+    'bg-white': !isOpen,
+    'outline outline-1 outline-sky-400': isChecked
+  });
 
   return (
     <AnimatePresence>
@@ -74,49 +78,41 @@ const Firm = ({firm}) => {
         transition={{ duration: 0.2 }}
       >
         <div className={classes}>
-          <div className="flex flex-col w-3/12 shrink-0">
-            <div className="flex justify-start items-center gap-1 text-xs">
-              <input
-                type="checkbox"
-                checked={isChecked}
-                onChange={handleCheckbox}
-              />
-              <label className="select-none" onClick={handleCheckbox}>
-                Incluir en la exportación
-              </label>
+          <div className='flex flex-col w-3/12 shrink-0'>
+            <div className='flex justify-start items-center gap-1 text-xs'>
+              <input type="checkbox" checked={isChecked} onChange={handleCheckbox} />
+              <label className='select-none' onClick={handleCheckbox}>Incluir en la exportación</label>
             </div>
-            <h2 className="select-none text-lg">{firm.name}</h2>
+            <h2 className='select-none text-lg'>{firm.name}</h2>
           </div>
 
-          <div className="flex justify-between gap-1 items-center w-8/12 px-2">
-            <div className="flex flex-col justify-center items-center">
-              <div className="font-light text-lg">{firm.rut}</div>
-              <div className="text-sm">Rut</div>
+          <div className='flex justify-between gap-1 items-center w-8/12 px-2'>
+            <div className='flex flex-col justify-center items-center'>
+              <div className='font-light text-lg'>{firm.rut}</div>
+              <div className='text-sm'>Rut</div>
             </div>
-            <div className="flex flex-col justify-center items-center w-1/5">
-              <div className="font-light text-lg text-center">
-                {firm.country}
-              </div>
-              <div className="text-sm">País</div>
+            <div className='flex flex-col justify-center items-center w-1/5'>
+              <div className='font-light text-lg text-center'>{firm.country}</div>
+              <div className='text-sm'>País</div>
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="font-light text-lg">{firm.sapCode || "-"}</div>
-              <div className="text-sm">SAP code</div>
+            <div className='flex flex-col justify-center items-center'>
+              <div className='font-light text-lg'>{firm.sapCode || '-'}</div>
+              <div className='text-sm'>SAP code</div>
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="font-light text-lg">-</div>
-              <div className="text-sm">Soc.Finales</div>
+            <div className='flex flex-col justify-center items-center'>
+              <div className='font-light text-lg'>-</div>
+              <div className='text-sm'>Soc.Finales</div>
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="font-light text-lg">-</div>
-              <div className="text-sm">Niveles</div>
+            <div className='flex flex-col justify-center items-center'>
+              <div className='font-light text-lg'>-</div>
+              <div className='text-sm'>Niveles</div>
             </div>
           </div>
 
-          <div className="flex justify-end align-middle w-1/12">
+          <div className='flex justify-end align-middle w-1/12'>
             <button onClick={toggleOpen}>
               <motion.div animate={{ rotate: isOpen ? 180 : 0 }}>
-                <ChevronDownIcon className="w-5 h-5 z-0" />
+                <ChevronDownIcon className='w-5 h-5 z-0' />
               </motion.div>
             </button>
           </div>
@@ -125,97 +121,70 @@ const Firm = ({firm}) => {
         <AnimatePresence>
           {isOpen && (
             <motion.div
-              className="flex flex-row p-6 bg-white z-10 rounded-b-md"
+              className='flex flex-row p-6 bg-white z-10 rounded-b-md'
               initial={{ opacity: 0, y: -5 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -5 }}
               transition={{ duration: 0.2 }}
             >
-              <div className="w-full">
+              <div className='w-full'>
                 <table class="w-full text-sm">
                   <thead>
-                    <tr className="border-b text-left">
-                      <th className="w-16 text-center">Nivel</th>
-                      <th className="w-20 text-center">%</th>
+                    <tr className='border-b text-left'>
+                      <th className='w-16 text-center'>Nivel</th>
+                      <th className='w-20 text-center'>%</th>
                       <th>Propietaria</th>
                       <th>Rut propietaria</th>
-                      <th className="w-2/5">Subsidiaria/s</th>
+                      <th className='w-2/5'>Subsidiaria/s</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr className="hover:bg-slate-100">
-                      <td className="text-center">1</td>
-                      <td className="text-center">10%</td>
+                    <tr className='hover:bg-slate-100'>
+                      <td className='text-center'>1</td>
+                      <td className='text-center'>10%</td>
                       <td>Lorem Ipsum</td>
                       <td>11.111.111-1</td>
-                      <td className="flex items-center gap-2">
-                        <div className="flex flex-wrap items-center gap-1 my-2">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-5 h-5 shrink-0"
-                          >
+                      <td className='flex items-center gap-2'>
+                        <div className='flex flex-wrap items-center gap-1 my-2'>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 shrink-0">
                             <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
-                            <path
-                              fillRule="evenodd"
-                              d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                              clipRule="evenodd"
-                            />
+                            <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
                           </svg>
-                          <span className="border p-1 bg-slate-100 rounded-md">
+                          <span className='border p-1 bg-slate-100 rounded-md'>
                             Nombre de la sociedad
                           </span>
                         </div>
                       </td>
                     </tr>
-                    <tr className="hover:bg-slate-100">
-                      <td className="text-center">1</td>
-                      <td className="text-center">10%</td>
+                    <tr className='hover:bg-slate-100'>
+                      <td className='text-center'>1</td>
+                      <td className='text-center'>10%</td>
                       <td>Lorem Ipsum</td>
                       <td>11.111.111-1</td>
-                      <td className="flex items-center gap-2">
-                        <div className="flex flex-wrap items-center gap-1 my-2">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-5 h-5 shrink-0"
-                          >
+                      <td className='flex items-center gap-2'>
+                        <div className='flex flex-wrap items-center gap-1 my-2'>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 shrink-0">
                             <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
-                            <path
-                              fillRule="evenodd"
-                              d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                              clipRule="evenodd"
-                            />
+                            <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
                           </svg>
-                          <span className="border p-1 bg-slate-100 rounded-md">
+                          <span className='border p-1 bg-slate-100 rounded-md'>
                             Nombre de la sociedad
                           </span>
                         </div>
                       </td>
                     </tr>
-                    <tr className="hover:bg-slate-100 border-t">
-                      <td className="text-center">2</td>
-                      <td className="text-center">10%</td>
+                    <tr className='hover:bg-slate-100 border-t'>
+                      <td className='text-center'>2</td>
+                      <td className='text-center'>10%</td>
                       <td>Lorem Ipsum</td>
                       <td>11.111.111-1</td>
-                      <td className="flex items-center gap-2">
-                        <div className="flex flex-wrap items-center gap-1 my-2">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-5 h-5 shrink-0"
-                          >
+                      <td className='flex items-center gap-2'>
+                        <div className='flex flex-wrap items-center gap-1 my-2'>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 shrink-0">
                             <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
-                            <path
-                              fillRule="evenodd"
-                              d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                              clipRule="evenodd"
-                            />
+                            <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
                           </svg>
-                          <span className="border p-1 bg-slate-100 rounded-md">
+                          <span className='border p-1 bg-slate-100 rounded-md'>
                             Nombre de la sociedad
                           </span>
                         </div>
@@ -232,4 +201,4 @@ const Firm = ({firm}) => {
   )
 })
 
-export default Firm;
+export default Firm
